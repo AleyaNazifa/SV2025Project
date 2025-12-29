6f872adf1a79b3e9ab4c831b9a38bd5c42930997
aleya nazifa <132155116+AleyaNazifa@users.noreply.github.com>
2025-12-29 20:37:07 +0800

    Modify render function to accept dataframe as argument

--- home.py
+++ home.py
@@ -1,15 +1,13 @@
 import streamlit as st
-from data_loader import get_df
 
-def render():
+def render(df):
     st.subheader("Home")
-    df = get_df()
-    if df is None:
-        st.stop()
+    st.write("Dataset overview and quick summary.")
 
-    c1, c2 = st.columns(2)
+    c1, c2, c3 = st.columns(3)
     c1.metric("Responses", f"{len(df)}")
     c2.metric("Columns", f"{df.shape[1]}")
+    c3.metric("Has Insomnia Index", "Yes" if "InsomniaSeverity_index" in df.columns else "No")
 
+    st.markdown("---")
     st.dataframe(df.head(30), use_container_width=True)
-
